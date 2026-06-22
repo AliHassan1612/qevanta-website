de3c0a24fd235a7e6800009e9f1fa3c690d35fc6
AliHassan1612 <alishyg16@gmail.com>
2026-06-23 00:38:35 +0500

    Update script.js

--- Qevanta Website/qevanta-pro-launch-website/script.js
+++ Qevanta Website/qevanta-pro-launch-website/script.js
@@ -1,5 +1,6 @@
 const SUPABASE_URL = "https://mfgmkvmawhfdsigawrpn.supabase.co";
-const SUPABASE_ANON_KEY = "PASTE_SUPABASE_ANON_KEY_HERE";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mZ21rdm1hd2hmZHNpZ2F3cnBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5Mjk5NjYsImV4cCI6MjA5NzUwNTk2Nn0.6znoxf1v7SDdvBvjMwgdDTjQt7dpsI2qbmvVI1BPN5c
+";
 let appliedReferral = null;
 async function applyReferral(){
   const code=document.getElementById('referralCode').value.trim();
