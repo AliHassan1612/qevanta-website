a600d556034a350b1d87ebf09588786977d0039b
AliHassan1612 <alishyg16@gmail.com>
2026-06-27 21:15:22 +0500

    Update supabase.js

--- admin/supabase.js
+++ admin/supabase.js
@@ -2,7 +2,7 @@ const QEVANTA_SUPABASE_URL = "https://mfgmkvmawhfdsigawrpn.supabase.co";
 
 const QEVANTA_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mZ21rdm1hd2hmZHNpZ2F3cnBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5Mjk5NjYsImV4cCI6MjA5NzUwNTk2Nn0.6znoxf1v7SDdvBvjMwgdDTjQt7dpsI2qbmvVI1BPN5c";
 
-const supabase = window.supabase.createClient(
+window.qevantaDb = window.supabase.createClient(
   QEVANTA_SUPABASE_URL,
   QEVANTA_SUPABASE_ANON_KEY
 );
